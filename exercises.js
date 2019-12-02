5465b31b32f3caffb43452e655fb69db4a05eb0c
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2019-12-02 20:05:41 +0000

    started

--- exercises.js
+++ exercises.js
@@ -1,4 +1,16 @@
-
+const cellphone = {
+  phoneNumber : '3473141826',
+  addContact: function(name,phoneNumber) {
+    cellphone.contacts.push({name, phoneNumber});
+    return `${name} successfully added`
+  }
+};
+cellphone.model = 'I-phone';
+cellphone.contacts = [];
+cellphone.contacts.push({name:'Paul',phoneNumber:'3473327880'});
+cellphone.addContact('My','1234567890');
+cellphone.addContact('Name','1234567890');
+cellphone.addContact('Dee','1234567890');
 
 module.exports = {
   cellphone,
