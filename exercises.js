b6d508ce81dbbe36bb0d4faceee2e78da058ed7c
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2019-12-03 15:30:22 +0000

    2 questions left and 2 short response left

--- exercises.js
+++ exercises.js
@@ -1,16 +1,42 @@
-const cellphone = {
-  phoneNumber : '3473141826',
-  addContact: function(name,phoneNumber) {
+const cellphone = { // Answer #1
+  phoneNumber : '3473141826', 
+  addContact: function(name,phoneNumber) { //Answer #5
     cellphone.contacts.push({name, phoneNumber});
     return `${name} successfully added`
-  }
+  },
+  numberOfContacts: function(){ //Answer #7
+    return this.contacts.length;
+  },
+  lookUp: function(str){
+    for(let key in this.contacts){
+      if(this.contacts[key].name === str) return this.contacts[key].phoneNumber
+    }
+    return 'Contact not found'
+  },
+  deleteContact: function(str){
+    for(let key in this.contacts){
+      if(this.contacts[key].name === str){
+        delete this.contacts[key]
+        return `${str} successfully deleted`
+      } 
+    }
+    return 'Contact not found';
+  },  
+  
 };
-cellphone.model = 'I-phone';
-cellphone.contacts = [];
-cellphone.contacts.push({name:'Paul',phoneNumber:'3473327880'});
-cellphone.addContact('My','1234567890');
-cellphone.addContact('Name','1234567890');
-cellphone.addContact('Dee','1234567890');
+cellphone.model = 'I-phone'; //Answer #2
+cellphone.contacts = []; // Answer #3
+cellphone.contacts.push({name:'Paul',phoneNumber:'3473327880'}); //Answer #4
+cellphone.addContact('My','1234567890'); // Answer #6
+cellphone.addContact('Name','1234567890'); // Answer #6
+cellphone.addContact('Dee','1234567890'); // Answer #6
+console.log(cellphone);
+console.log(cellphone.numberOfContacts());
+
+
+
+
+
 
 module.exports = {
   cellphone,
