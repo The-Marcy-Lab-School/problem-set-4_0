642eddc6c010f3252fb3e4c1f9bd9114898fa44a
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2023-02-02 10:20:46 -0500

    Updating tests
    `cellphoneCopy` was being created using `{...cellphone}` which only spreads own properties of `cellphone`. If the fellow chooses to create an object using `Object.create()` and methods are inherited from a prototype, those methods won't be added. The update addresses this.

--- problems/exercises.test.js
+++ problems/exercises.test.js
@@ -22,7 +22,8 @@ test('Question 4: contact was added to contacts property', () => {
 });
 
 test('Question 5: addContact method adds to contacts array', () => {
-  let cellphoneCopy = {...cellphone};
+  let cellphoneCopy = Object.create(Object.getPrototypeOf(cellphone));
+  Object.assign(cellphoneCopy, cellphone);
   cellphoneCopy.contacts = [];
   let contactCount = cellphoneCopy.contacts.length;
   expect(cellphoneCopy.addContact).not.toBeUndefined();
@@ -40,7 +41,8 @@ test('Question 6: Contacts were added to contacts property', () => {
 });
 
 test('Question 7: numberOfContacts works', () => {
-  let cellphoneCopy = {...cellphone};
+  let cellphoneCopy = Object.create(Object.getPrototypeOf(cellphone));
+  Object.assign(cellphoneCopy, cellphone);
   cellphoneCopy.contacts = [];
   expect(cellphoneCopy.addContact).not.toBeUndefined();
   expect(cellphoneCopy.addContact('Reuben Ogbonna', '9196219388')).toBe('Reuben Ogbonna successfully added.');
@@ -51,7 +53,8 @@ test('Question 7: numberOfContacts works', () => {
 });
 
 test('Question 8: lookUp works', () => {
-  let cellphoneCopy = {...cellphone};
+  let cellphoneCopy = Object.create(Object.getPrototypeOf(cellphone));
+  Object.assign(cellphoneCopy, cellphone);
   cellphoneCopy.contacts = [];
   expect(cellphoneCopy.addContact).not.toBeUndefined();
   expect(cellphoneCopy.addContact('Reuben Ogbonna', '9196219388')).toBe('Reuben Ogbonna successfully added.');
@@ -62,19 +65,21 @@ test('Question 8: lookUp works', () => {
 });
 
 test('Question 9: deleteContact works', () => {
-  let cellphoneCopy = {...cellphone};
+  let cellphoneCopy = Object.create(Object.getPrototypeOf(cellphone));
+  Object.assign(cellphoneCopy, cellphone);
   cellphoneCopy.contacts = [];
-  cellphoneCopy.contacts.push({name: 'Jim Smith', phoneNumber: '9876543210'});
   expect(cellphoneCopy.addContact).not.toBeUndefined();
   expect(cellphoneCopy.addContact('Reuben Ogbonna', '9196219388')).toBe('Reuben Ogbonna successfully added.');
   expect(cellphoneCopy.addContact('Ann', '0987654321')).toBe('Ann successfully added.');
+  cellphoneCopy.contacts.push({name: 'Jim Smith', phoneNumber: '9876543210'});
   expect(cellphoneCopy.deleteContact('Jim Smith')).toBe('Jim Smith successfully deleted.');
   expect(cellphoneCopy.contacts.some((contact) => contact.name === 'Jim Smith')).toBe(false);
   expect(cellphoneCopy.deleteContact(Math.random().toString())).toBe('Contact not found.');
 });
 
 test('Question 10: call works for name', () => {
-  let cellphoneCopy = {...cellphone};
+  let cellphoneCopy = Object.create(Object.getPrototypeOf(cellphone));
+  Object.assign(cellphoneCopy, cellphone);
   cellphoneCopy.contacts = [];
   expect(cellphoneCopy.addContact).not.toBeUndefined();
   expect(cellphoneCopy.addContact('Reuben Ogbonna', '9196219388')).toBe('Reuben Ogbonna successfully added.');
@@ -87,9 +92,6 @@ test('Question 10: call works for name', () => {
 
 test('Question 11: makeCellPhone factory function', () => {
   const reubensCell = makeCellPhone('9196219388', 'iPhone XR');
-  const annesCell = makeCellPhone('1111111111', 'Flip Phone');
-  expect(annesCell === reubensCell).toBe(false);
-  
   expect(typeof reubensCell).toBe("object");
   expect(reubensCell.phoneNumber).toBe('9196219388');
   expect(reubensCell.model).toBe('iPhone XR');
